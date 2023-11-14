1975ecd525de80df6463895158be5e091f72b4f7
MintCat <presidentmc9898@gmail.com>
2023-11-14 13:43:23 +0900

    [Add] Implement a Const. and a Dest. of `Customer` Class

--- Src/Customer.cpp
+++ Src/Customer.cpp
@@ -1,2 +1,38 @@
 #include "Customer.h"
 
+Customer::Customer(string name) {
+	// Set name
+	this->customerName = name;
+	// Lists initialized
+	for (int i = 0; i < 10; i++) {
+		bankList[i] = nullptr;
+	}
+	for (int i = 0; i < 100; i++) {
+		cardList[i] = nullptr;
+	}
+	cout << "[Const-C] A new user is added.\n";
+}
+Customer::~Customer() {
+	cout << "[Dest-C] User " << this->customerName << "is deleted.\n";
+}
+void Customer::getName() {
+	
+}
+void Customer::newAccount() {
+	
+}
+void Customer::deleteAccount() {
+
+}
+void Customer::getAccountList() {
+
+}
+void Customer::newCard() {
+
+}
+void Customer::deleteCard() {
+
+}
+void Customer::getCardList() {
+
+}
--- Src/Customer.h
+++ Src/Customer.h
@@ -1,22 +1,19 @@
 #pragma once
 
 #include "Bank.h"
-#include "Account.h"
 #include "Card.h"
 
 class Bank;
-class Account;
 class Card;
 
 class Customer {
 private:
 	string customerName;
 	Bank* bankList[10];
-	Account* accountList[100];
 	Card* cardList[100];
 public:
 	// Customer
-	Customer();
+	Customer(string name);
 	~Customer();
 	void getName();
 	// Account
