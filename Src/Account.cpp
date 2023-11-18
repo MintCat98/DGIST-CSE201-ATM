a59b9e0e62dc07a4e5f32e8da657fe04134190df
YoungJin <dd@YoungJins-MacBookPro.local>
2023-11-18 21:47:04 +0900

    Account.cpp added

--- Src/Account.cpp
+++ Src/Account.cpp
@@ -1,2 +1,52 @@
 #include "Account.h"
+#include <iostream>
+using namespace std;
+
+Account::Account() {
+    accountNum = 0;
+    primaryBank = nullptr;
+    ownedBy = nullptr;
+    balance = 0;
+    accountHistory= new Transaction() // I'm not sure
+    cout << "[Const-A] A new account is created.\n";
+}
+
+Account::~Account() {
+    delete accountHistory;
+    cout << "[Dest-A] Account is deleted.\n";
+}
+
+int Account::getAccountNum() const {
+    cout << "Account Number: " << accountNum << "\n";
+    return accountNum;
+}
+
+void Account::getBankName() const{
+    string bankName = "Not Assigned";
+    if (primaryBank != nullptr) {
+        bankName = primaryBank->getName();
+    }
+    cout << "Bank Name: " << bankName << "\n";
+}
+void Account::getOwner() const {
+    string userName = "Not Assigned";
+    if ( ownedBy != nullptr) {
+        userName = ownedBy->getName();
+    }
+    cout << "User Name: " << userName << "\n";
+}
+
+int Account::getBalance() const {
+    return balance;
+}
+
+void Account::addTransaction(Transaction* transaction) {
+
+}
+void Account::updateBalance() {
+
+}
+void Account::updateHistory() {
+
+}
 
